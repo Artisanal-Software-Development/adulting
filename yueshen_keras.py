02b63b76795f950823f50cc27f26ebc345bd2cd7
Yue Shen Gu <yueshengu14@gmail.com>
2017-03-14 22:39:07 -0700

    update python script and add notebook

--- yueshen_keras.py
+++ yueshen_keras.py
@@ -14,19 +14,19 @@ test_response = numpy.loadtxt('data/cleaned/test_response.csv', delimiter=",", s
 
 # create model
 model = Sequential()
-model.add(Dense(12, input_dim=8, init='uniform', activation='relu'))
-model.add(Dense(8, init='uniform', activation='relu'))
+model.add(Dense(30162, input_dim=64, init='uniform', activation='relu'))
+model.add(Dense(64, init='uniform', activation='relu'))
 model.add(Dense(1, init='uniform', activation='sigmoid'))
-
 # Compile model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])              
 
 # Fit the model
-model.fit(train_x, train_response, nb_epoch=150, batch_size=10)
+model.fit(train_x, train_response, nb_epoch=10, batch_size=3000)
 
 # evaluate the model
 scores = model.evaluate(train_x, train_response)
 print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+# acc: 75.11%
 
 # calculate predictions
 predictions = model.predict(test_x)
@@ -34,8 +34,5 @@ predictions = model.predict(test_x)
 # round predictions
 rounded = [round(x[0]) for x in predictions]
 print(rounded)
-
-if __name__ == "__main__":
-  print(prediction)
 
 
